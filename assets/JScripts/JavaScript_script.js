ddc0d8dd0baf040bc7933513a3bcc5c7122085b9
PaulKnees <jakewallace368@gmail.com>
2022-11-30 18:42:57 +0000

    JS for toggles elements

--- assets/JScripts/JavaScript_script.js
+++ assets/JScripts/JavaScript_script.js
@@ -1,1 +1,29 @@
-// var arrMethButt = document.getElementById('arrMeths')
+// var arraySection = document.getElementById('arraySection')
+var arrayMethodButton = document.getElementById('arrayMethodButton')
+var arrayInfoButton = document.getElementById('arrayInfoButton')
+var arrayInfo = document.getElementById('arrayInfo')
+var arrayMethodInfo = document.getElementById('arrayMethodInfo')
+
+
+
+
+arrayInfoButton.addEventListener('click', toInfo)
+arrayMethodButton.addEventListener('click', toMethod);
+
+
+function toMethod(){
+arrayInfoButton.style.display = 'block';
+arrayInfo.style.display = 'block';
+arrayMethodButton.style.display = 'none';
+arrayMethodInfo.style.display = 'none';
+}
+function toInfo() {
+    arrayInfoButton.style.display = 'none';
+    arrayInfo.style.display = 'none';
+    arrayMethodButton.style.display = 'block';
+    arrayMethodInfo.style.display = 'block';
+
+    
+}
+
+
