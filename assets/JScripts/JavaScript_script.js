8344b917f626d302e4fda13822c65be1b5becc7d
PaulKnees <jakewallace368@gmail.com>
2022-11-30 20:22:44 +0000

    fixes button

--- assets/JScripts/JavaScript_script.js
+++ assets/JScripts/JavaScript_script.js
@@ -2,28 +2,26 @@
 var arrayMethodButton = document.getElementById('arrayMethodButton')
 var arrayInfoButton = document.getElementById('arrayInfoButton')
 var arrayInfo = document.getElementById('arrayInfo')
-var arrayMethodInfo = document.getElementById('arrayMethodInfo')
+var arrayMethod = document.getElementById('arrayMethod')
 
 
+var arrayMethod = document.getElementById('arrayInfo')
+var arrayInfo = document.getElementById('arrayMethod')
 
-
-arrayInfoButton.addEventListener('click', toInfo)
 arrayMethodButton.addEventListener('click', toMethod);
-
+arrayInfoButton.addEventListener('click', toInfo)
 
 function toMethod(){
 arrayInfoButton.style.display = 'block';
 arrayInfo.style.display = 'block';
 arrayMethodButton.style.display = 'none';
-arrayMethodInfo.style.display = 'none';
+arrayMethod.style.display = 'none';
 }
 function toInfo() {
     arrayInfoButton.style.display = 'none';
     arrayInfo.style.display = 'none';
     arrayMethodButton.style.display = 'block';
-    arrayMethodInfo.style.display = 'block';
-
-    
+    arrayMethod.style.display = 'block';   
 }
 
 
