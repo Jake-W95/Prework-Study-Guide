dc6906d0fb75543e8c3e32dfa19549921c873614
PaulKnees <jakewallace368@gmail.com>
2023-01-25 23:49:16 +0000

    styling changes using JS

--- assets/JScripts/main_script.js
+++ assets/JScripts/main_script.js
@@ -1,29 +1,4 @@
-var topics = ["HTML", "CSS", "Git", "Bash Commands", "Javascript"];
-
-var RandomTopic = topics[Math.floor(Math.random() * topics.length)];
-
-function ListTopics() {
-    for(var x = 0; x < topics.length; x++) {
-        console.log(topics[x]);
-    }
+let links = document.querySelectorAll('a');
+for(link of links){
+    link.classList.add("inner", 'border')
 }
-
-function SelectTopics() {
-    if (RandomTopic === 'HTML') {
-        console.log("Let's study HTML!");
-       } else if (RandomTopic === 'CSS') {
-        console.log("Let's study CSS!");
-       } else if (RandomTopic === 'Git') {
-        console.log("Let's study Git!");
-       } else if (RandomTopic === 'Bash Commands') {
-        console.log("Let's study Bash Commands!");
-       } else if (RandomTopic === 'JavaScript') {
-        console.log("Let's study JavaScript!");
-       } else {
-        console.log('Please try again!');
-       }
-    }
-console.log("Here are the topics learned throughout prework");
-ListTopics()
-console.log("Which topic should we study first");
-SelectTopics()
